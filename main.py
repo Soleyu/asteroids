c6208dc49c383e796aef0138c3df2935439bdd4b
Soleyu <soleyu.alpha@gmail.com>
2025-01-10 02:53:47 +0100

    Added Clock

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    main_clock = pygame.time.Clock()
+    delta_time_value = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, "#000000")
         pygame.display.flip()
+        tick_value = main_clock.tick(60)
+        delta_time_value = tick_value / 1000
 
 if __name__ == "__main__":
     main()
